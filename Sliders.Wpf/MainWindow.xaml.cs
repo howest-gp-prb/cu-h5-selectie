82ab634e275c02db6ba17383a56cad26bd2c9e9d
Dries Deboosere <dries.deboosere@howest.be>
2019-09-17 22:49:55 +0200

    Add project files.

---
+++ Sliders.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+
+namespace Sliders.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTestSlider_Click(object sender, RoutedEventArgs e)
+        {
+            int getal1 = (int)sldGetal1.Value;
+            int getal2 = (int)sldGetal2.Value;
+            string boodschap = "";
+
+            if (getal1 == getal2)
+            {
+                boodschap = "eerste = tweede";
+            }
+            else if (getal1 < getal2)
+            {
+                boodschap = "eerste < tweede";
+            }
+            else
+            {
+                boodschap = "eerste > tweede";
+            }
+
+            lblBoodschap.Content = boodschap;
+        }
+    }
+}
