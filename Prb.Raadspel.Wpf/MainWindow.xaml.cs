83cc6c3791da16c03abe3a4aad8fbe3dcbf8d79e
Stefaan Vercaemer <stefaan.vercaemer@howest.be>
2020-09-12 16:36:35 +0200

    Convert to .Net Core projects

---
+++ Prb.Raadspel.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prb.Raadspel.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int teRaden;
+        int pogingen;
+        Random rnd = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            teRaden = rnd.Next(1, 11);
+            Debug.WriteLine("Te raden: " + teRaden);
+            pogingen = 0;
+        }
+
+        private void BtnGok_Click(object sender, RoutedEventArgs e)
+        {
+            int gok = int.Parse(txtGok.Text);
+            if (gok == teRaden)
+            {
+                pogingen++;
+                lblGokjes.Content = "Je hebt het getal " + teRaden + " geraden na " + pogingen + " pogingen";
+            }
+            else
+            {
+                lblGokjes.Content += gok.ToString() + Environment.NewLine;
+                txtGok.Text = "";
+                txtGok.Focus();
+                pogingen++;
+            }
+        }
+    }
+}
---
+++ Prb.Sliders.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Prb.Sliders.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnTestSlider_Click(object sender, RoutedEventArgs e)
+        {
+            int getal1 = (int)sldGetal1.Value;
+            int getal2 = (int)sldGetal2.Value;
+            string boodschap = "";
+
+            if (getal1 == getal2)
+            {
+                boodschap = "eerste = tweede";
+            }
+            else if (getal1 < getal2)
+            {
+                boodschap = "eerste < tweede";
+            }
+            else
+            {
+                boodschap = "eerste > tweede";
+            }
+
+            lblBoodschap.Content = boodschap;
+        }
+    }
+}
