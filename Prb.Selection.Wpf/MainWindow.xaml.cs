a86ea374b9f2db478567ff9eddddc8f68695dfa7
Herman De Beukelaer <herman.de.beukelaer@howest.be>
2021-09-20 20:14:44 +0200

    Translate variables

--- Prb.Selection.Wpf/MainWindow.xaml.cs
+++ Prb.Selection.Wpf/MainWindow.xaml.cs
@@ -27,29 +27,29 @@ namespace Prb.Selectie.Wpf
 
         private void IsEvening()
         {
-            int uur = 19;
-            bool avond = (uur >= 18) && (uur <= 22);
-            MessageBox.Show("Om " + uur + " uur: " + avond.ToString(), "Is het al avond?");
+            int hour = 19;
+            bool evening = (hour >= 18) && (hour <= 22);
+            MessageBox.Show("Om " + hour + " uur: " + evening, "Is het al avond?");
         }
 
         private void IsNight()
         {
-            int uur = 23;
-            bool nacht = (uur > 22) || (uur < 6);
-            MessageBox.Show("Om " + uur + " uur: " + nacht.ToString(), "Is het nacht?");
+            int hour = 23;
+            bool night = (hour > 22) || (hour < 6);
+            MessageBox.Show("Om " + hour + " uur: " + night, "Is het nacht?");
         }
 
         private void BtnTestBool_Click(object sender, RoutedEventArgs e)
         {
-            bool benjeklaar = false;
-            MessageBox.Show(benjeklaar.ToString(), "Ben je klaar?");
+            bool areYourReady = false;
+            MessageBox.Show(areYourReady.ToString(), "Ben je klaar?");
         }
 
         private void BtnTestNot_Click(object sender, RoutedEventArgs e)
         {
-            bool benjeklaar = false;
-            benjeklaar = !benjeklaar;
-            MessageBox.Show(benjeklaar.ToString(), "Ben je klaar?");
+            bool areYouReady = false;
+            areYouReady = !areYouReady;
+            MessageBox.Show(areYouReady.ToString(), "Ben je klaar?");
         }
 
         private void BtnCheckEvening_Click(object sender, RoutedEventArgs e)
@@ -64,81 +64,80 @@ namespace Prb.Selectie.Wpf
 
         private void BtnWeekDay_Click(object sender, RoutedEventArgs e)
         {
-            string dagNaam;
-            DateTime momenteel = DateTime.Now;
-            DayOfWeek dag = momenteel.DayOfWeek;
-            if (dag == DayOfWeek.Sunday)
-                dagNaam = "zondag";
-            else if (dag == DayOfWeek.Monday)
-                dagNaam = "maandag";
-            else if (dag == DayOfWeek.Tuesday)
-                dagNaam = "dinsdag";
-            else if (dag == DayOfWeek.Wednesday)
-                dagNaam = "woensdag";
-            else if (dag == DayOfWeek.Thursday)
-                dagNaam = "donderdag";
-            else if (dag == DayOfWeek.Friday)
-                dagNaam = "vrijdag";
-            else dagNaam = "zaterdag";
-            MessageBox.Show("Vandaag is het " + dagNaam, "Dag van de week");
+            string dayName;
+            DateTime currentDate = DateTime.Now;
+            DayOfWeek currentDay = currentDate.DayOfWeek;
+            if (currentDay == DayOfWeek.Sunday)
+                dayName = "zondag";
+            else if (currentDay == DayOfWeek.Monday)
+                dayName = "maandag";
+            else if (currentDay == DayOfWeek.Tuesday)
+                dayName = "dinsdag";
+            else if (currentDay == DayOfWeek.Wednesday)
+                dayName = "woensdag";
+            else if (currentDay == DayOfWeek.Thursday)
+                dayName = "donderdag";
+            else if (currentDay == DayOfWeek.Friday)
+                dayName = "vrijdag";
+            else dayName = "zaterdag";
+            MessageBox.Show("Vandaag is het " + dayName, "Dag van de week");
         }
 
         private void BtnTicketPrice_Click(object sender, RoutedEventArgs e)
         {
-            int leeftijd = 30;
-            int ticketPrijs;
+            int age = 30;
+            int ticketPrice;
             string beroep = "ambtenaar";
-
 
             if (beroep == "werkloos" || beroep == "gepensioneerd")
             {
-                ticketPrijs = 0;
+                ticketPrice = 0;
             }
             else
             {
-                if (leeftijd < 12)
+                if (age < 12)
                 {
-                    ticketPrijs = 0;
+                    ticketPrice = 0;
                 }
                 else
                 {
-                    ticketPrijs = 12;
+                    ticketPrice = 12;
                 }
             }
 
-            MessageBox.Show("De prijs van je ticket bedraagt: " + ticketPrijs + " euro");
+            MessageBox.Show("De prijs van je ticket bedraagt: " + ticketPrice + " euro");
         }
 
         private void BtnWeekDaySwitch_Click(object sender, RoutedEventArgs e)
         {
-            string dagNaam;
-            DateTime momenteel = DateTime.Now;
-            DayOfWeek dag = momenteel.DayOfWeek;
-            switch (dag)
+            string dayName;
+            DateTime currentDate = DateTime.Now;
+            DayOfWeek currentDay = currentDate.DayOfWeek;
+            switch (currentDay)
             {
                 case DayOfWeek.Sunday:
-                    dagNaam = "zondag";
+                    dayName = "zondag";
                     break;
                 case DayOfWeek.Monday:
-                    dagNaam = "maandag";
+                    dayName = "maandag";
                     break;
                 case DayOfWeek.Tuesday:
-                    dagNaam = "dinsdag";
+                    dayName = "dinsdag";
                     break;
                 case DayOfWeek.Wednesday:
-                    dagNaam = "woensdag";
+                    dayName = "woensdag";
                     break;
                 case DayOfWeek.Thursday:
-                    dagNaam = "donderdag";
+                    dayName = "donderdag";
                     break;
                 case DayOfWeek.Friday:
-                    dagNaam = "vrijdag";
+                    dayName = "vrijdag";
                     break;
                 default:
-                    dagNaam = "zaterdag";
+                    dayName = "zaterdag";
                     break;
             }
-            MessageBox.Show("Vandaag is het " + dagNaam, "Dag van de week");
+            MessageBox.Show("Vandaag is het " + dayName, "Dag van de week");
         }
     }
 }
